1d4d6a463c451aa2e45b88363c8cd78c8badaf05
hukuuu <hukuuu@gmail.com>
2014-09-07 16:54:58 +0300

    openshift added

--- app/js/modules/config/constants.js
+++ app/js/modules/config/constants.js
@@ -1,3 +1,5 @@
 angular.module('config')
-  .constant('baseUrl', 'http://localhost:8080/restaurants/api/')
-  .constant('baseImgUrl', 'http://localhost:8080/restaurants/resources/images');
+  // .constant('baseUrl', 'http://localhost:8080/restaurants/api/')
+  // .constant('baseImgUrl', 'http://localhost:8080/restaurants/resources/images');
+  .constant('baseUrl','http://restaurants-hukuuu.rhcloud.com/restaurants/api/')
+  .constant('baseImgUrl', 'http://restaurants-hukuuu.rhcloud.com/restaurants/resources/images');
